8aca1d41e4e0b55d239fbf066437fbb4872b435a
踏梦去远方 <lxjyzzq@163.com>
2023-10-23 17:50:16 +0800

    更新内容

--- src/main/java/file/ListFilesDemo2.java
+++ src/main/java/file/ListFilesDemo2.java
@@ -5,7 +5,7 @@ import java.io.File;
  * 重载的listFiles方法,允许我们传入一个文件过滤器FileFilter
  * 该方法会将目录中所有满足过滤要求的子项返回,不满足的则被忽略
  */
-public interface ListFilesDemo2 {
+public class ListFilesDemo2 {
     public static void main(String[] args) {
         //将当前项目目录下所有名字以"."开始的子项获取
         File dir = new File(".");
