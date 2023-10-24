ca7ac3ee0907034e72b9e87734b56d6ada1fae51
踏梦去远方 <lxjyzzq@163.com>
2023-10-24 11:07:30 +0800

    更新内容

--- src/main/java/file/test5.java
+++ src/main/java/file/test5.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class test5 {
     public static void main(String[] args) {
         File dir = new File("demo");
-        delFile(dir);
+        delFile5(dir);
         System.out.println("删除成功");
     }
 
@@ -20,6 +20,73 @@ public class test5 {
         if(file.isDirectory()){
             File[] subs = file.listFiles();
             for (File sub: subs) {
+                delFile(sub);
+            }
+            file.delete();
+        }
+        file.delete();
+    }
+
+    //第五遍
+    public static void delFile5(File file){
+        if(file.isDirectory()){
+            File[] subs = file.listFiles();
+            for (File sub:
+                 subs) {
+                delFile5(sub);
+            }
+        }
+        file.delete();
+    }
+
+    //第四遍
+    public static void delFile4(File file){
+        if(file.isDirectory()){
+            File[] subs = file.listFiles();
+            for (File sub:
+                 subs) {
+                delFile4(sub);
+            }
+            file.delete();
+        }
+        file.delete();
+    }
+
+    //第三遍
+    public static void delFile3(File file) {
+        if (file.isDirectory()) {
+            File[] subs = file.listFiles();
+            for (File sub :
+                    subs) {
+                delFile3(sub);
+            }
+            file.delete();
+        }
+        file.delete();
+    }
+
+
+    //第二遍
+    public static void delFile2(File file){
+        if(file.isDirectory()){
+            File[] subs = file.listFiles();
+            for (File sub : subs) {
+                delFile2(sub);
+            }
+            file.delete();
+        }
+        file.delete();
+    }
+
+
+
+
+
+    //第一遍
+    public static void delFile1(File file){
+        if(file.isDirectory()){
+            File[] subs = file.listFiles();
+            for (File sub: subs ) {
                 delFile(sub);
             }
             file.delete();
