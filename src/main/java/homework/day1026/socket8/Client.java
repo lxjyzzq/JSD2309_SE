0b63bdbd77a46c79d507d10096701105f5f1b58f
踏梦去远方 <lxjyzzq@163.com>
2023-10-26 20:48:46 +0800

    更新内容

---
+++ src/main/java/homework/day1026/socket8/Client.java
@@ -0,0 +1,34 @@
+package homework.day1026.socket8;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    public Client(){
+        try {
+            System.out.println("正在连接服务端.....");
+            socket = new Socket("127.0.0.1", 5888);
+            System.out.println("连接服务端成功......");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start(){
+        try {
+            OutputStream out = socket.getOutputStream();
+            out.write(8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
+}
