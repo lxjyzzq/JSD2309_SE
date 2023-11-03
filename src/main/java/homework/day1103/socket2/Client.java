2bbdecd332eae69c536e637fad6e90a1090e6ae6
踏梦去远方 <lxjyzzq@163.com>
2023-11-03 20:19:32 +0800

    更新内容

---
+++ src/main/java/homework/day1103/socket2/Client.java
@@ -0,0 +1,87 @@
+package homework.day1103.socket2;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * 聊天室客户端
+ */
+public class Client {
+    private Socket socket;
+    public Client(){
+        try {
+            System.out.println("正在连接服务端...");
+            socket = new Socket("localhost",8088);
+            System.out.println("服务端已连接");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void start(){
+        try {
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter(bw,true);
+            Scanner scanner = new Scanner(System.in);
+            String nickName;
+            while(true) {
+                System.out.println("请输入您的昵称:");
+                nickName = scanner.nextLine();
+                if(nickName.trim().length()>0){
+                    pw.println(nickName);//将昵称发送给服务端
+                    break;
+                }else{
+                    System.out.println("昵称至少输入一个文字");
+                }
+            }
+            System.out.println("欢迎你["+nickName+"],开始聊天吧!");
+
+            ServerHandler handler = new ServerHandler();
+            Thread t = new Thread(handler);
+            t.setDaemon(true);
+            t.start();
+
+            while(true) {
+                String line = scanner.nextLine();
+                if("exit".equalsIgnoreCase(line)){
+                    break;
+                }
+                pw.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
+
+    private class ServerHandler implements Runnable{
+        public void run(){
+            try {
+                InputStream in = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(in,StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+
+                String message;
+                while((message = br.readLine())!=null){
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
